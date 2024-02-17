f5acf032647fcb4bc92a803f7d72186cb0cc403e
Himanshu Prasad <himanshuprasad468@gmail.com>
2024-02-17 11:28:01 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "30691b0d98mshbfddbe4beab394fp10338ejsn83c630f34710",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "b08296126f50dc7cae8bf3d503f76ddf"; // enter your key from openweather API
